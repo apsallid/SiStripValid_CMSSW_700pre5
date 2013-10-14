461904f00703272ee7c27928a094ad49778df1ac
Andreas Psallidas <andreas.psallidas@cern.ch>
2013-10-14 17:20:09 +0200

    keep only TrackerRecHits and RecoTrack to this branch

--- DQM/SiStripCommon/python/__init__.py
+++
@@ -1,3 +0,0 @@
-#Automatically created by SCRAM
-import os
-__path__.append(os.path.dirname(os.path.abspath(__file__).rsplit('/DQM/SiStripCommon/',1)[0])+'/cfipython/slc5_amd64_gcc481/DQM/SiStripCommon')
--- DQMServices/Components/python/DQMFastTimerServiceClient_cfi.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-dqmFastTimerServiceClient = cms.EDAnalyzer('FastTimerServiceClient',
-                                           dqmPath = cms.untracked.string( "DQM/TimerService" )
-                                           )
